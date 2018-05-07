3b824a6ab7e926039386d590991989cb7712e745
matthewdhyatt <36710098+matthewdhyatt@users.noreply.github.com>
2018-05-06 23:12:26 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,17 @@
-# https://github.com/Rapptz/discord.py/blob/async/examples/reply.py
 import discord
+from discord.ext import commands
 
-TOKEN = 'NDQyODEyMTI5ODcyMDUyMjI1.DdERKw.-L130qG6A1pMKIq8xK7Az8oqnpo'
+bot = commands.Bot(command_prefix='$')
 
-client = discord.Client()
-
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-client.run(TOKEN)
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+bot.run('<YOUR_TOKEN_HERE>')
