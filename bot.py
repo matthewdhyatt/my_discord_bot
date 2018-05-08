7882542a1e298ee5657326b565b745e39fc8ae89
matthewdhyatt <36710098+matthewdhyatt@users.noreply.github.com>
2018-05-08 10:44:06 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,37 +64,3 @@ async def _bot():
     await bot.say('Yes, the bot is cool.')
 
 bot.run(token)
-
-
-
-
-
-'''
-# https://github.com/Rapptz/discord.py/blob/async/examples/reply.py
-import discord
-import os
-
-token=os.environ['TOKEN']
-#os.environ.get('TOKEN')
-
-client = discord.Client()
-
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-client.run(token)
-'''
